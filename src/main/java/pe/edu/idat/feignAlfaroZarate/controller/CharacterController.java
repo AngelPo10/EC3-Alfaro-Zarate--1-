cae5754e98a7d95dd07cd8ade0a93f695094171b
AngelPo10 <zaratemiguel346@gmail.com>
2025-03-16 12:59:40 -0500

    controller final

--- src/main/java/pe/edu/idat/feignAlfaroZarate/controller/CharacterController.java
+++ src/main/java/pe/edu/idat/feignAlfaroZarate/controller/CharacterController.java
@@ -1,5 +1,27 @@
 package pe.edu.idat.feignAlfaroZarate.controller;
 
-public class CharacterController {
+import org.springframework.web.bind.annotation.*;
+import pe.edu.idat.feignAlfaroZarate.client.placeholder.iuserservice.Character;
+import pe.edu.idat.feignAlfaroZarate.service.ICharacterService;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/character")
+public class CharacterController {
+    private final ICharacterService characterService;
+
+    public CharacterController(ICharacterService characterService) {
+        this.characterService = characterService;
+    }
+    //este es el endpoint para que nos todos los personajes
+    @GetMapping
+    public List<Character> getCharacters() {
+        return characterService.obtenerTodosLosPersonajes();
+    }
+    //este el endpoint obtener un personaje por id
+    @GetMapping("/{id}")
+    public Character getCharacterById(@PathVariable Long id) {
+        return characterService.obtenerPersonajePorId(id);
+    }
 }
